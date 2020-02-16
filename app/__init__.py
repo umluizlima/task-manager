595f4fa00b488b724e1c191d3e427b322f74e3b3
Luiz Lima <umluizlima@gmail.com>
2020-02-15 22:23:37 -0300

    Add task creation functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,25 @@
+from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel, constr
+from uuid import UUID, uuid4
 
 TASKS = []
+
+
+class TaskStatus(str, Enum):
+    OPEN = "OPEN"
+    CLOSED = "CLOSED"
+
+
+class TaskInput(BaseModel):
+    title: constr(min_length=3, max_length=50)
+    description: constr(max_length=140)
+    status: TaskStatus = TaskStatus.OPEN
+
+
+class Task(TaskInput):
+    id: UUID
+
 
 app = FastAPI()
 
@@ -8,3 +27,11 @@ app = FastAPI()
 @app.get("/tasks")
 def list():
     return TASKS
+
+
+@app.post('/tasks', response_model=Task, status_code=201)
+def create(task: TaskInput):
+    new_task = task.dict()
+    new_task.update({"id": uuid4()})
+    TASKS.append(new_task)
+    return new_task
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -52,3 +52,85 @@ def test_listed_task_should_have_status():
     response = client.get("/tasks")
     assert "status" in response.json().pop()
     TASKS.clear()
+
+
+def test_task_endpoint_should_accept_post():
+    client = TestClient(app)
+    response = client.post("/tasks")
+    assert response.status_code != 405
+
+
+def test_task_should_have_title():
+    client = TestClient(app)
+    response = client.post("/tasks", json={})
+    assert response.status_code == 422
+
+
+def test_task_title_should_have_at_least_3_characters():
+    client = TestClient(app)
+    response = client.post("/tasks", json={"title": 2 * "*"})
+    assert response.status_code == 422
+
+
+def test_task_title_should_have_at_most_50_characters():
+    client = TestClient(app)
+    response = client.post("/tasks", json={"title": 51 * "*"})
+    assert response.status_code == 422
+
+
+def test_task_should_have_description():
+    client = TestClient(app)
+    response = client.post("/tasks", json={"title": "Title"})
+    assert response.status_code == 422
+
+
+def test_task_description_should_have_at_most_140_characters():
+    client = TestClient(app)
+    response = client.post(
+        "/tasks",
+        json={"title": "Title", "description": "*" * 141})
+    assert response.status_code == 422
+
+
+def test_created_task_should_be_returned():
+    client = TestClient(app)
+    task = {"title": "Title", "description": "Description"}
+    response = client.post("/tasks", json=task)
+    assert response.json()["title"] == task["title"]
+    assert response.json()["description"] == task["description"]
+    TASKS.clear()
+
+
+def test_created_task_should_have_unique_id():
+    client = TestClient(app)
+    task1 = {"title": "Title 1", "description": "Description1"}
+    task2 = {"title": "Title 2", "description": "Description2"}
+    response1 = client.post("/tasks", json=task1)
+    response2 = client.post("/tasks", json=task2)
+    assert response1.json()["id"] != response2.json()["id"]
+    TASKS.clear()
+
+
+def test_created_task_should_have_default_status_open():
+    client = TestClient(app)
+    task = {"title": "Title", "description": "Description"}
+    response = client.post("/tasks", json=task)
+    assert response.json()["status"] == "OPEN"
+    TASKS.clear()
+
+
+def test_created_task_should_return_status_201():
+    client = TestClient(app)
+    task = {"title": "Titulo", "description": "Description"}
+    response = client.post("/tasks", json=task)
+    assert response.status_code == 201
+    TASKS.clear()
+
+
+def test_created_task_should_be_persisted():
+    client = TestClient(app)
+    task = {"title": "Title", "description": "Description"}
+    response = client.post("/tasks", json=task)
+    assert response.status_code == 201
+    assert len(TASKS) == 1
+    TASKS.clear()
