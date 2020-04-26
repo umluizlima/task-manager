819cb4235dc210e6b28abb5b8321a101ca796317
umluizlima <umluizlima@gmail.com>
2020-04-26 18:32:14 -0300

    Add persistence conditioned on feature flag

--- app/database.py
+++ app/database.py
@@ -1,7 +1,34 @@
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
 db = {
     "tasks": [],
 }
 
 
+def setup_db():
+    SQLALCHEMY_DATABASE_URL = getenv(
+        "DATABASE_URL", default="postgresql://postgres:postgres@localhost/task-manager",
+    )
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    return SessionLocal
+
+
+Base = declarative_base()
+
+
 def get_db():
-    yield db
+    if not getenv("PERSISTENCE_ENABLED", default=False):
+        yield db
+    SessionLocal = setup_db()
+    try:
+        session = SessionLocal()
+        yield session
+    finally:
+        session.close()
